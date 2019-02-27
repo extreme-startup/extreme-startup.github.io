45fad16ff843fd2aa8f912c16082f17d9fae5f1b
Anatolii Karelov <anatolii.karelov@kingfisher.com>
2019-02-27 16:40:33 +0000

    Add http client and env dependent BASE_URLs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
+import axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL || process.env.BASE_URL;
+
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
 new Vue({
